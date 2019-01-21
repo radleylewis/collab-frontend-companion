399a350ab95bfa52c1747b26cdcc649144bed085
rad <radley.lewis@gmail.com>
2019-01-21 16:36:16 +0100

    feat: setup for ngrx integration

---
+++ src/app/app.actions.ts
@@ -0,0 +1,5 @@
+import { Action } from '@ngrx/store';
+
+export enum ActionTypes {
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WalletsPageComponent } from './wallets-page/wallets-page.component';
 import { VotingPageComponent } from './voting-page/voting-page.component';
 import { LoginComponent } from './login/login.component';
+import { AppReducer } from './app.reducer';
 
 import { GetWalletsService } from './get-wallets.service';
 
@@ -21,6 +23,7 @@ import { GetWalletsService } from './get-wallets.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule, 
+    StoreModule.forRoot({ app: AppReducer })
   ],
   providers: [
     GetWalletsService
