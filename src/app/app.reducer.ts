9bdf888a98ce9087c7f64dce955c7506688e41bd
Edward Neuwirth <edward.neuwirth@outlook.com>
2019-01-21 19:26:17 +0100

    chore: added NgRX

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,19 +1,31 @@
-import { Action } from '@ngrx/store';
-import { ActionTypes } from './app.actions';
+import { ActionTypes, Login } from "./app.actions";
 
-export const initialState = 0;
- 
-export function AppReducer(state = initialState, action: Action) {
+
+export interface State {
+  username: string;
+  password: string;
+  bearer: string;
+  wallets: object[];
+}
+
+export const initialState: State = {
+  username: "",
+  password: "",
+  bearer: "",
+  wallets: []
+};
+
+export function AppReducer(state = initialState, action: Login) {
   switch (action.type) {
-    // case ActionTypes.Increment:
-    //   return state + 1;
- 
-    // case ActionTypes.Decrement:
-    //   return state - 1;
- 
-    // case ActionTypes.Reset:
-    //   return 0;
- 
+    case ActionTypes.Login:
+      console.log('username, ', action.payload.username);
+      console.log('password, ', action.payload.password);
+      return {
+        ...state,
+        username: action.payload.username,
+        password: action.payload.password
+      };
+
     default:
       return state;
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from "@angular/core";
-
+import { Store, select } from "@ngrx/store";
+import { Login } from "../app.actions";
+import { State } from "../app.reducer"
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private store: Store<State>) {}
   user = {
-    username: '',
-    password: ''
+    username: "",
+    password: ""
   };
+
+  click(username: string, password: string) {
+    this.store.dispatch(new Login({ username: username, password: password }));
+  }
+
   ngOnInit() {}
 }
-
